3dbbb58afc69d8a7168ce089704a8db77836e349
Hayward <hpeirce@gmail.com>
2019-04-01 20:48:50 -0700

    HP: setup flask port and host

--- index.py
+++ index.py
@@ -159,4 +159,4 @@ def index():
     return render_template('index.html', computers = getComputerList())
  
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000, threaded=True, host=('0.0.0.0'))
